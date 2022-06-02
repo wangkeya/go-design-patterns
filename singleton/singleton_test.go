ab365ed6ffed463fda73fdc81a906f8ed941c29a
wangkeya <783242528@qq.com>
2022-06-02 15:12:08 +0800

    补充用例

--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -6,12 +6,15 @@ import (
 )
 
 func TestNewHttpClient(t *testing.T) {
-	client := NewHttpClient()
-	res, err := client.Get("https://www.baidu.com")
-	if err != nil {
-		t.Error("http get failed")
+	for i := 0; i < 100; i++ {
+		client := NewHttpClient()
+		res, err := client.Get("https://www.baidu.com")
+		if err != nil {
+			t.Error("http get failed")
+		}
+		if res.StatusCode != http.StatusOK {
+			t.Error("http get status code failed")
+		}
 	}
-	if res.StatusCode != http.StatusOK {
-		t.Error("http get status code failed")
-	}
+
 }
